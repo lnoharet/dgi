5e333814004b3a1cf631bc5f7407ed07c3c9bb3d
lnoharet <78497146+lnoharet@users.noreply.github.com>
2022-06-22 17:38:50 +0200

    Added UI menu and other fixes

--- NS_fluid.cs
+++ NS_fluid.cs
@@ -1,4 +1,3 @@
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,6 +12,9 @@ public class NS_fluid : MonoBehaviour
     static private int h = 64;
     static private int w = 128;
 
+    // obstacles bool matrix
+    private bool [,] obstacles = new bool[w,h]; 
+
     // density
     private float [,] dens = new float[w,h]; 
     private float [,] dens_prev = new float[w,h]; 
@@ -25,10 +27,16 @@ public class NS_fluid : MonoBehaviour
     private float [,] v = new float[w,h]; 
     private float [,] v_prev = new float[w,h]; 
 
-    private float viscosity = 0.0f;
-    private float densdiff = 0.0f;
-    private float force = 1.0f;
+    [Header("Simulation Parameters")]
+    public float viscosity = 0f;
+    public float densdiff = 0.0f;
+    public float force = 1.0f;
+    public float source = 50.0f;
+    public bool static_source = true;
+    public bool borders = false; 
 
+
+    private float velocity_decrease = 1f;
     private Vector3 mouse_pos;
     private Vector2Int deltacoords;
     private Vector2Int coords;
@@ -48,13 +56,12 @@ public class NS_fluid : MonoBehaviour
         prev_coords = new Vector2Int(-1, -1);
 		float iii = Math.Max(h*1.0f, w/1.7f);
 		this.gameObject.transform.localScale = new Vector3((float)w/iii, 0.0001f,(float)h/iii);
-		this.gameObject.transform.position = new Vector3(0,0,0);
 		
-        planemask = LayerMask.GetMask("UI");
+        planemask = LayerMask.GetMask("Plane");
         smoke_texture = new Texture2D(w, h);
         GetComponent<Renderer>().material.mainTexture = smoke_texture; 
         smoke_texture.wrapMode = TextureWrapMode.Clamp;
-        smoke_texture.filterMode = FilterMode.Point;
+        //smoke_texture.filterMode = FilterMode.Point;
 
         for (int i = 0; i < w; i++){
             for (int j=0 ; j < h ; j++ ){
@@ -62,25 +69,102 @@ public class NS_fluid : MonoBehaviour
                 u[i,j] = 0f;
                 v[i,j] = 0f;
                 dens_prev[i,j] = 0f;
+                obstacles[i,j] = false;
             }
         }
+        if (static_source){
+            set_obstacles();
+        }
+        
         to_texture();
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {       
+    public void set_obstacles(){
+        
+        /*for (int i = 0; i < w; i++){
+            for (int j=0 ; j < h ; j++ ){
+                if (i < w/4+5 && i > w/4-5 && j < h/4+5 && i > h/4-5 ){
+                    obstacles[i,j] = true;
 
-		if(Input.GetMouseButton(2)){
-            for (int i = 0; i < w; i++){
-                for (int j=0 ; j < h ; j++ ){
-                    dens[i,j] = 0f;
-                    u[i,j] = 0f;
-                    v[i,j] = 0f;
-                    dens_prev[i,j] = 0f;
                 }
             }
+        }
+        */
+        
+        //Circle
+        float circle_size = h/8;
+
+        int center_x = 1*w/4;
+        int center_y = 1*h/2;
+        for (int i = 0; i < w; i++){
+            for (int j=0 ; j < h ; j++ ){
+                int dx = i - center_x;
+                int dy = j - center_y;
+                double distance = Math.Sqrt((dx * dx) + (dy * dy));
+                
+                //Testing for obstacle creation fix
+                double d2 = Math.Sqrt(((dx-1) * (dx-1)) + (dy * dy));
+                double d3 = Math.Sqrt(((dx+1) * (dx+1)) + (dy * dy));
+                double d4 = Math.Sqrt((dx * dx) + ((dy-1) * (dy-1)));
+                double d5 = Math.Sqrt((dx * dx) + ((dy+1) * (dy+1)));
+
+                
+
+                //testing done; remove && and onwards if not a problem
+                if(distance < (float) circle_size ){
+                    obstacles[i, j] = true;
+                }
+
+            }
+        }
+        
+        
+        
+        
+    }
+
+    public void clear_scene(){
+        for (int i = 0; i < w; i++){
+            for (int j=0 ; j < h ; j++ ){
+                dens[i,j] = 0f;
+                u[i,j] = 0f;
+                v[i,j] = 0f;
+                dens_prev[i,j] = 0f;
+                obstacles[i,j] = false;
+                
+                
+            }
+        }
+        if (static_source){
+            set_obstacles();
+        } 
+    }
+    public void clear_boarders(){
+        for (int j = 0; j < h; j++){ 
+                dens[0,j]   = 0f; 
+                dens[w-1,j] = 0f; 
+                u[0,j] = 0f;
+                v[0,j] = 0f;
+                u[w-1,j] = 0f;   
+                v[w-1,j] = 0f;         
+        }
+        for (int i = 0; i < w; i++){
+                dens[i,0]   = 0f;
+                dens[i,h-1] = 0f; 
+                u[i,0] = 0f;
+                v[i,0] = 0f;
+                u[i,h-1] = 0f; 
+                v[i,h-1] = 0f;    
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {    
+        
+		if(Input.GetMouseButton(2)){
+            clear_scene();
         }
         //Vector3 mousepos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         //Debug.Log(mousepos);
@@ -94,7 +178,6 @@ public class NS_fluid : MonoBehaviour
                 v_prev[i,j] = 0f;
             }
         }   
-
         //Set source: TODO
         if(Input.GetMouseButton(0) || Input.GetMouseButton(1)){
             // Get user input as vec3
@@ -104,49 +187,86 @@ public class NS_fluid : MonoBehaviour
             if (Physics.Raycast(ray, out hit, 100f, planemask))
             {   
                 //Debug.Log("Test");
-            	//Debug.Log(hit.point);
+                //Debug.Log(hit.point);
 
                 //Vector2 tempcoords = new Vector2((((hit.point.x + 5)*w / 10)), (((hit.point.z + 5)*h / 10)));
-				//Vector2 tempcoords = new Vector2(((hit.point.x)*w), ((hit.point.y)*h));
+                //Vector2 tempcoords = new Vector2(((hit.point.x)*w), ((hit.point.y)*h));
                 
-				float iii = Math.Max(h*1.0f, w/1.7f);
-				Vector2 tempcoords = new Vector2((hit.point.x/((5f*(float)w)/iii))*((float)w/2)+((float)w/2), (hit.point.y/((5f*(float)h)/iii))*((float)h/2)+((float)h/2));
-				//Vector2 tempcoords = new Vector2((hit.point.x+0.5f)*w, (hit.point.y+0.5f)*h);
-				
-				//float iii = Math.Max(h*1.0f, w/1.8f);
-				//Vector2 tempcoords = new Vector2(((hit.point.x+5f)*(float)w/iii)+0.5f, ((hit.point.y+5f)*(float)h/iii)+0.5f);
+                float iii = Math.Max(h*1.0f, w/1.7f);
+                Vector2 tempcoords = new Vector2((hit.point.x/((5f*(float)w)/iii))*((float)w/2)+((float)w/2), (hit.point.y/((5f*(float)h)/iii))*((float)h/2)+((float)h/2));
+                //Vector2 tempcoords = new Vector2((hit.point.x+0.5f)*w, (hit.point.y+0.5f)*h);
+                
+                //float iii = Math.Max(h*1.0f, w/1.8f);
+                //Vector2 tempcoords = new Vector2(((hit.point.x+5f)*(float)w/iii)+0.5f, ((hit.point.y+5f)*(float)h/iii)+0.5f);
 
                 coords.x = (int) tempcoords.x;
                 coords.y = (int) tempcoords.y;
 
- 
-                //if(coords.x > 0 && coords.x < w-1 && coords.y > 0 && coords.y < h-1){
-				//Set density source
-				if(Input.GetMouseButton(0)){
-					dens_prev[coords.x, coords.y] = 100f;
-				}
-				
-				if (Input.GetMouseButton(1)){
-					if(prev_coords.x != -1){
-						deltacoords = new Vector2Int(coords.x - prev_coords.x, coords.y - prev_coords.y);
-	
-						u_prev[coords.x, coords.y] = deltacoords.x*force*w;
-						v_prev[coords.x, coords.y] = deltacoords.y*force*h;
-						Debug.Log(u_prev[coords.x, coords.y]);
-                		Debug.Log(v_prev[coords.x, coords.y]);
-					}
-					prev_coords.x = coords.x;
-					prev_coords.y = coords.y;
-				}
+
+                if(coords.x > 0 && coords.x < w-1 && coords.y > 0 && coords.y < h-1){
+                    //Set density source
+                    if(Input.GetMouseButton(0) && !obstacles[coords.x, coords.y]){
+                        //TODO: testing
+                        dens_prev[coords.x, coords.y] = Math.Max(source - dens[coords.x, coords.y], dens_prev[coords.x, coords.y]);
+                    }
+                    
+                    if (Input.GetMouseButton(0)){
+                        if(prev_coords.x != -1 && !obstacles[coords.x, coords.y]){
+                            deltacoords = new Vector2Int(coords.x - prev_coords.x, coords.y - prev_coords.y);
+        
+                            u_prev[coords.x, coords.y] = deltacoords.x*force*100;
+                            v_prev[coords.x, coords.y] = deltacoords.y*force*100;
+                            Debug.Log(u_prev[coords.x, coords.y]);
+                            Debug.Log(v_prev[coords.x, coords.y]);
+                        }
+                        prev_coords.x = coords.x;
+                        prev_coords.y = coords.y;
+                    }
+                    if(Input.GetMouseButton(1)){
+                        int coordsx1;
+                        int coordsx2;
+                        int coordsy1;
+                        int coordsy2;
+                        
+                        if (tempcoords.x - coords.x > 0.5){
+                            coordsx1 = coords.x;
+                            coordsx2 = coords.x+1;
+                        } else{
+                            coordsx1 = coords.x-1;
+                            coordsx2 = coords.x;
+                        }
+                        if (tempcoords.y - coords.y > 0.5){
+                            coordsy1 = coords.y;
+                            coordsy2 = coords.y+1;
+                        } else{
+                            coordsy1 = coords.y-1;
+                            coordsy2 = coords.y;
+                        }
+                        
+                        obstacles[coordsx1, coordsy1] = true;
+                        obstacles[coordsx1, coordsy2] = true;
+                        obstacles[coordsx2, coordsy1] = true;
+                        obstacles[coordsx2, coordsy2] = true;
+
+                    }
                                     
-                //}
+                }
                 //Debug.Log(deltacoords.x);
                 
             }
         }
-        if(!Input.GetMouseButton(1)){
+        if(!Input.GetMouseButton(0)){
             prev_coords.x = -1;
             prev_coords.y = -1;
+        }
+        if (static_source){
+            //add_static_source(1,h/2 + h/9);
+            //add_static_source(1,h/2 - h/9);
+            add_static_source(1,h/2 - h/16);
+            add_static_source(1,h/2 + h/16);
+            add_static_source(1,h/2 + h/6);
+            add_static_source(1,h/2 - h/6);
+
         }
 
         // Add source to velocity
@@ -166,20 +286,11 @@ public class NS_fluid : MonoBehaviour
         u = advect(1, u, u_prev, u_prev, v_prev, dt);
         v = advect(2, v, v_prev, u_prev, v_prev, dt);
         
-
         var proj_tup = project(u, v, u_prev, v_prev);
         u = proj_tup.Item1;
         v = proj_tup.Item2;
         u_prev = proj_tup.Item3;
         v_prev = proj_tup.Item4;
-
-        //TEMP:  TODO
-
-        //for(int i=0; i<w; i++){
-        //    for(int j=0; j<h; j++){
-        //        Debug.Log(u_prev[i, j]);
-        //    }
-        //}
 
         // Update density
         add_source_dens(dt);
@@ -217,8 +328,15 @@ public class NS_fluid : MonoBehaviour
         {
             for (int x = 0; x < smoke_texture.width; x++)
             {
-                //smoke_texture.SetPixel(x, y, new Color((u[x,y]+v[x,y])*dens[x,y],(u[x,y]+v[x,y])*dens[x,y],(u[x,y]+v[x,y])*dens[x,y],1));
-                smoke_texture.SetPixel(x, y, new Color(dens[x,y],dens[x,y],dens[x,y],1));
+                if (obstacles[x,y] == true){
+                    smoke_texture.SetPixel(x, y, new Color(0,1,0,1));
+                    
+                }
+                else {
+                    //smoke_texture.SetPixel(x, y, new Color((u[x,y]+v[x,y])*dens[x,y],(u[x,y]+v[x,y])*dens[x,y],(u[x,y]+v[x,y])*dens[x,y],1));
+                    smoke_texture.SetPixel(x, y, new Color(dens[x,y],dens[x,y],dens[x,y],1));
+                }
+                
             }
         }
         smoke_texture.Apply();
@@ -276,32 +394,113 @@ public class NS_fluid : MonoBehaviour
     }
 
     float[,] set_boundaries(int b, float[,] m){
-        int i;
-        for (i = 1; i < h-1; i++){ 
-            if (b==1) { 
-                m[0,i]   = -m[1,i]; 
-                m[w-1,i] = -m[w-2,i]; 
-            }
-            else { 
-                m[0,i]   = m[1,i]; 
-                m[w-1,i] = m[w-2,i]; 
-            }
-		}
-		for (i = 1; i < w-1; i++){
-            if (b==2) { 
-                m[i,0]   = -m[i,1];
-                m[i,h-1] = -m[i,h-2]; 
-            }
-            else { 
-                m[i,0]   = m[i,1];
-                m[i,h-1] = m[i,h-2]; 
+        int i,j;
+
+        for (i = 1; i < w-1; i++){ 
+            for (j = 1; j < h-1; j++){ 
+                if (obstacles[i,j]){
+                    if(b==1){
+                        if(!obstacles[i-1,j]){
+                            // cell to left is not obstacle
+                            m[i,j]   = -m[i-1,j] * velocity_decrease;
+                        }
+                        else if(!obstacles[i+1,j]){
+                            // cell to right is not obstacle
+                            m[i,j]   = -m[i+1,j] * velocity_decrease;
+                        }
+                        else{
+                            // middle obstacle cell
+                            m[i,j] = 0f;
+                        }
+
+                    }  
+                    else if(b==2){
+                        if(!obstacles[i,j-1]){
+                            // cell down is not obstacle
+                            m[i,j]   = -m[i,j-1] * velocity_decrease;
+                        }
+                        else if(!obstacles[i,j+1]){
+                            // cell up is not obstacle
+                            m[i,j]   = -m[i,j+1] * velocity_decrease;
+                        }
+                        else{
+                            // middle obstacle cell
+                            m[i,j] = 0f;
+                        }
+                           
+                    } 
+                    else{
+                        // set density in obstacle
+                        if(!obstacles[i,j-1]){
+                            // cell down is not obstacle
+                            m[i,j]   = m[i,j-1];
+                        }
+                        else if(!obstacles[i,j+1]){
+                            // cell up is not obstacle
+                            m[i,j]   = m[i,j+1];
+                        }
+                        else if(!obstacles[i-1,j]){
+                            // cell to left is not obstacle
+                            m[i,j]   = m[i-1,j];
+                        }
+                        else if(!obstacles[i+1,j]){
+                            // cell to right is not obstacle
+                            m[i,j]   = m[i+1,j];
+                        }
+                        else{
+                            m[i,j] = 0f;
+                        }
+                    }
+                    // if (b==1 || b==2){
+                    //     // Set obtacle corners
+                    //     if(!obstacles[i+1,j] && !obstacles[i,j+1] && !obstacles[i+1,j+1]){
+                    //         // TOP RIGHT CORNER
+                    //         m[i+1,j+1] = (m[i,j+1] + m[i+1,j])/2 ;
+                    //     }   
+                    //     else if(!obstacles[i+1,j] && !obstacles[i,j-1] && !obstacles[i+1,j-1]){
+                    //         // BOTTOM RIGHT CORNER
+                    //         m[i+1,j-1] = (m[i,j-1] + m[i+1,j])/2 ;
+                    //     }
+                    //     else if(!obstacles[i-1,j] && !obstacles[i,j-1] && !obstacles[i-1,j-1]){
+                    //         // BOTTOM LEFT CORNER
+                    //         m[i-1,j-1] = (m[i,j-1] + m[i-1,j])/2 ;
+                    //     }
+                    //     else if(!obstacles[i-1,j] && !obstacles[i,j+1] && !obstacles[i-1,j+1]){
+                    //         // TOP LEFT CORNER
+                    //         m[i-1,j+1] = (m[i,j+1] + m[i-1,j])/2 ;
+                    //     }               
+                    // }
+                }
             }
         }
-        m[0,0]     = 0.5f*(m[1,0]+ m[0,1]);
-        m[0,h-1]   = 0.5f*(m[1,h-1]+ m[0,h-2]);
-        m[w-1,0]   = 0.5f*(m[w-2,0]+ m[w-1,1]);
-        m[w-1,h-1] = 0.5f*(m[w-2,h-1]+ m[w-1,h-2]);
-        
+        if (borders){
+            for (i = 1; i < h-1; i++){ 
+                if (b==1) { 
+                    m[0,i]   = -m[1,i]; 
+                    m[w-1,i] = -m[w-2,i]; 
+                    
+                }
+                else { 
+                    m[0,i]   = m[1,i]; 
+                    m[w-1,i] = m[w-2,i]; 
+                }
+                
+            }
+            for (i = 1; i < w-1; i++){
+                if (b==2) { 
+                    m[i,0]   = -m[i,1];
+                    m[i,h-1] = -m[i,h-2]; 
+                }
+                else { 
+                    m[i,0]   = m[i,1];
+                    m[i,h-1] = m[i,h-2]; 
+                }
+            }
+            m[0,0]     = 0.5f*(m[1,0]+ m[0,1]);
+            m[0,h-1]   = 0.5f*(m[1,h-1]+ m[0,h-2]);
+            m[w-1,0]   = 0.5f*(m[w-2,0]+ m[w-1,1]);
+            m[w-1,h-1] = 0.5f*(m[w-2,h-1]+ m[w-1,h-2]);
+        }
         return m;
     }
 
@@ -336,10 +535,13 @@ public class NS_fluid : MonoBehaviour
         u_curr = set_boundaries(1, u_curr);
         v_curr = set_boundaries(2, v_curr);
 
-        //Return value
-        //var returntuple = Tuple.Create(u_curr, v_curr);
-        //return returntuple;
         return (u_curr, v_curr, u_prev_, v_prev_);
+    }
+
+    void add_static_source(int source_x, int source_y){
+        dens_prev[source_x, source_y] = source;
+        u_prev[source_x, source_y] = force*w;
+        v_prev[source_x, source_y] = 0f;
     }
 
 
